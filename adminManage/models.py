c9ba22fccc1fb8a1c4f15b171c0a296c5f59ee76
bazingayu <bazingayu@tencent.com>
2019-01-03 20:31:43 +0800

    login接口完成

--- adminManage/models.py
+++ adminManage/models.py
@@ -1,6 +1,171 @@
-# -*- coding: utf-8 -*-
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Admin(models.Model):
+    aid = models.AutoField(primary_key=True)
+    user_id = models.CharField(unique=True, max_length=128)
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    salt = models.CharField(max_length=255)
+    access_token = models.CharField(max_length=255, blank=True, null=True)
+    token_expires_in = models.IntegerField(blank=True, null=True)
+    create_time = models.IntegerField()
+    status = models.TextField()  # This field type is a guess.
+    last_login_time = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'admin'
+        unique_together = (('aid', 'user_id'),)
+
+
+class Article(models.Model):
+    aid = models.AutoField(primary_key=True)
+    id = models.CharField(unique=True, max_length=128)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    category_id = models.CharField(max_length=128, blank=True, null=True)
+    create_time = models.IntegerField()
+    delete_time = models.IntegerField(blank=True, null=True)
+    update_time = models.IntegerField(blank=True, null=True)
+    publish_time = models.IntegerField(blank=True, null=True)
+    status = models.IntegerField(blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+    html_content = models.TextField(blank=True, null=True)
+    cover = models.TextField(blank=True, null=True)
+    sub_message = models.TextField(blank=True, null=True)
+    pageview = models.IntegerField(blank=True, null=True)
+    is_encrypt = models.TextField(blank=True, null=True)  # This field type is a guess.
+
+    class Meta:
+        managed = False
+        db_table = 'article'
+        unique_together = (('aid', 'id'),)
+
+
+class ArticleTagMapper(models.Model):
+    article_id = models.CharField(max_length=128)
+    tag_id = models.CharField(max_length=128)
+    create_time = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'article_tag_mapper'
+
+
+class BlogConfig(models.Model):
+    blog_name = models.CharField(max_length=255, blank=True, null=True)
+    avatar = models.TextField(blank=True, null=True)
+    sign = models.TextField(blank=True, null=True)
+    wxpay_qrcode = models.TextField(blank=True, null=True)
+    alipay_qrcode = models.TextField(blank=True, null=True)
+    github = models.TextField(blank=True, null=True)
+    view_password = models.CharField(max_length=255, blank=True, null=True)
+    salt = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'blog_config'
+
+
+class Category(models.Model):
+    aid = models.AutoField(primary_key=True)
+    id = models.CharField(unique=True, max_length=128)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    create_time = models.IntegerField()
+    update_time = models.IntegerField(blank=True, null=True)
+    status = models.TextField(blank=True, null=True)  # This field type is a guess.
+    article_count = models.IntegerField(blank=True, null=True)
+    can_del = models.TextField()  # This field type is a guess.
+
+    class Meta:
+        managed = False
+        db_table = 'category'
+        unique_together = (('aid', 'id'),)
+
+
+class Comments(models.Model):
+    article_id = models.CharField(max_length=128)
+    parent_id = models.IntegerField()
+    reply_id = models.IntegerField(blank=True, null=True)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=128, blank=True, null=True)
+    content = models.TextField()
+    source_content = models.TextField(blank=True, null=True)
+    create_time = models.IntegerField()
+    delete_time = models.IntegerField(blank=True, null=True)
+    status = models.IntegerField(blank=True, null=True)
+    is_author = models.TextField(blank=True, null=True)  # This field type is a guess.
+
+    class Meta:
+        managed = False
+        db_table = 'comments'
+
+
+class Friends(models.Model):
+    aid = models.AutoField(primary_key=True)
+    friend_id = models.CharField(unique=True, max_length=128)
+    name = models.CharField(max_length=255)
+    url = models.TextField()
+    create_time = models.IntegerField()
+    update_time = models.IntegerField(blank=True, null=True)
+    delete_time = models.IntegerField(blank=True, null=True)
+    status = models.TextField()  # This field type is a guess.
+    type_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'friends'
+        unique_together = (('aid', 'friend_id'),)
+
+
+class FriendsType(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    count = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'friends_type'
+
+
+class Pages(models.Model):
+    type = models.CharField(max_length=128)
+    md = models.TextField(blank=True, null=True)
+    html = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'pages'
+
+
+class SysLog(models.Model):
+    time = models.IntegerField()
+    content = models.TextField(blank=True, null=True)
+    ip = models.CharField(max_length=30, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'sys_log'
+
+
+class Tag(models.Model):
+    aid = models.AutoField(primary_key=True)
+    id = models.CharField(unique=True, max_length=128)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    create_time = models.IntegerField()
+    update_time = models.IntegerField(blank=True, null=True)
+    status = models.TextField(blank=True, null=True)  # This field type is a guess.
+    article_count = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tag'
+        unique_together = (('aid', 'id'),)
--- adminManage/views.py
+++ adminManage/views.py
@@ -5,62 +5,93 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from adminManage import models
+
+import json
+import hashlib
+import time
+import uuid
+
+def cb_passwordEqual(hash_value, salt, password):
+    hash = hashlib.md5()
+    hash.update(password + salt)
+    return hash.hexdigest() == hash_value
+
 def login(request):
-    #检查用户名是否存在
     ret = dict()
-    $isEx = $this->db->where('username', $username)->count_all_results(TABLE_ADMIN);
-    if $isEx == 0 :
+    if request.method == 'POST':
+        #检查用户名是否存在
+        parm = request.POST
+        username_value = parm.get('username','not found')
+        isEx = models.Admin.objects.filter(username=username_value).count()
+        if isEx == 0:
+    	    ret["status"] = False
+    	    ret["code"] = -1
+    	    ret["msg"] = '该账号不存在'
+    	    ret['data'] = []
+            return HttpResponse(json.dumps(ret))
+        #查询账号信息
+        userinfo = models.Admin.objects.get(username=username_value)
+        if ord(userinfo.status) != 0:
+    	    ret["status"] = False
+    	    ret["code"] = -1
+    	    ret["msg"] = '账号异常'
+    	    ret['data'] = []
+            return HttpResponse(json.dumps(ret))
+        #比对密码
+        if cb_passwordEqual(userinfo.password, userinfo.salt, parm.get('password','not found')) == False:
+    	    ret["status"] = False
+    	    ret["code"] = -1
+    	    ret["msg"] = '密码错误'
+    	    ret['data'] = []
+            return HttpResponse(json.dumps(ret))
+        #更新登陆时间
+        cur_time = int(time.time())
+        access_token = str(uuid.uuid1())
+        data = {
+            "last_login_time": cur_time,
+            "access_token": str(uuid.uuid1()),
+            "token_expires_in": cur_time + 60 * 60 * 24 * 7,
+        }
+        #更新数据
+        models.Admin.objects.filter(username=username_value).update(**data)
+
+        userinfo = models.Admin.objects.get(username=username_value)
+        retdata = {
+            "userId": userinfo.user_id,
+            "userName": userinfo.username,
+            "lastLoginTime": userinfo.last_login_time,
+            "token": {
+                         "accessToken": userinfo.access_token,
+                         "tokenExpiresIn": userinfo.token_expires_in,
+                         "exp": 60 * 60 * 24 * 7, 
+            },
+        }
+    	ret["status"] = True
+    	ret["code"] = 0
+    	ret["msg"] = '登陆成功'
+    	ret['data'] = retdata
+        return HttpResponse(json.dumps(ret))
+    else:
     	ret["status"] = False
     	ret["code"] = -1
     	ret["msg"] = '该账号不存在'
     	ret['data'] = []
-        return ret
+        return HttpResponse(json.dumps(ret))
 
-        $adminInfo = $this->db->where('username', $username)
-                            ->from(TABLE_ADMIN)
-                            ->get()
-                            ->row_array();
-
-        switch($adminInfo['status']) {
-            case '0':
-                break;
-            default:
-                return fail('账号异常');
-        }
-
-        if (!cb_passwordEqual($adminInfo['password'], $adminInfo['salt'], $password)) {
-            return fail('密码错误');
-        }
-
-        $time = time();
-
-        $data = array(
-            'last_login_time' => $time,
-            'access_token' => create_id(),
-            'token_expires_in' => $time + WEEK
-        );
-
-        // 更新数据
-        $this->db->where('username', $username)->update(TABLE_ADMIN, $data);
-
-        $adminInfo = $this->db->where('username', $username)
-                            ->from(TABLE_ADMIN)
-                            ->get()
-                            ->row_array();
-
-        $userResult = array(
-            'userId' => $adminInfo['user_id'],
-            'userName' => $adminInfo['username'],
-            'lastLoginTime' => $adminInfo['last_login_time'],
-            'token' => array(
-                'accessToken' => $adminInfo['access_token'],
-                'tokenExpiresIn' => $adminInfo['token_expires_in'],
-                'exp' => WEEK
-            )
-        );
-        
-        return success($userResult);
-    return HttpResponse("Hello, world. You're at the polls index.")
+    #    $userResult = array(
+    #        'userId' => $adminInfo['user_id'],
+    #        'userName' => $adminInfo['username'],
+    #        'lastLoginTime' => $adminInfo['last_login_time'],
+    #        'token' => array(
+    #            'accessToken' => $adminInfo['access_token'],
+    #            'tokenExpiresIn' => $adminInfo['token_expires_in'],
+    #            'exp' => WEEK
+    #        )
+    #    );
+    #    
+    #    return success($userResult);
+    #return HttpResponse("Hello, world. You're at the polls index.")
 
 def get_qiniu_token(request):
     return HttpResponse("Hello, world. You're at the polls index.")
